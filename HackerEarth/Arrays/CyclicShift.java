9fd4c0847c2f7f22a7815f62c436922b4b9a1701
Shaik-Muj <mesmh0810@gmail.com>
2025-10-15 12:38:36 +0530

    Updated code

--- HackerEarth/Arrays/CyclicShift.java
+++ HackerEarth/Arrays/CyclicShift.java
@@ -18,29 +18,59 @@ class CyclicShift{
     }
 
     static void helper(String binary, int n, int k){
-        int count = 0;
-        String max = generateMaximum(binary,n);
-        String temp = binary;
+        char[] digits = binary.toCharArray();
+        String max = binary;
 
-        for(int i=0; i<n; i++){
-            temp = temp.substring(1) + temp.charAt(0);
-            if(!temp.equals(max)) count++;
-            
-        }
-
-        System.out.println((2*count)+1);
-    }
-
-    static String generateMaximum(String binary, int n) {
-        String maxBinary = binary;
-        String temp = binary;
-        for(int i = 0; i < n; i++) {
-            temp = temp.substring(1) + temp.charAt(0);
-            if(temp.compareTo(maxBinary) > 0) {
-                maxBinary = temp;
+        for(int shift=1; shift<n; shift++){
+            if(isGreater(digits, shift, max,n)){
+                max = buildMax(digits, shift, n);
             }
         }
-    
-        return maxBinary;
+
+        List<Integer> positions = new ArrayList<>(); 
+        for (int shift = 0; shift < n; shift++) { 
+            if (equalsMax(max, digits, n, shift)) { 
+                positions.add(shift); 
+            } 
+        }
+
+        int countInOneCycle = positions.size();
+        if(countInOneCycle == 0){
+            System.out.println(0); 
+            return;
+        }
+
+        long numCycles = (k-1)/countInOneCycle;
+        int index = (int)(k-1)%countInOneCycle;
+
+        long res = (numCycles*n) + positions.get(index);
+
+        System.out.println(res);
+
+    }
+
+    static boolean isGreater(char[] digits,int shift,String max,int n){
+        for(int i=0; i<n; i++){
+            char c1 = max.charAt(i);
+            char c2 = digits[(i+shift)%n];
+            if(c1 != c2) return c1 < c2;
+        }
+
+        return false;
+    }
+
+    static String buildMax(char[] digits, int shift, int n){
+        StringBuilder sb = new StringBuilder(n);
+        for(int i=0; i<n; i++){
+            sb.append(digits[(i+shift)%n]);
+        }
+        return sb.toString();
+    }
+
+    static boolean equalsMax(String max, char[] digits, int n, int shift){
+        for(int i=0; i<n; i++){
+            if(digits[(i+shift)%n] != max.charAt(i)) return false;
+        }
+        return true;
     }
 }
